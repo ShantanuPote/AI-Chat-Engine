3a94e58d1363749cba3567b8493945e4c7667333
ShantanuPote <shantanupote40@mail.com>
2026-03-14 17:27:05 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# AI Chat Application
+
+A real-time AI chat application similar to ChatGPT.  
+This project allows users to interact with an AI model through a real-time chat interface.
---
+++ src/db/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+
+async function connectDB(){
+    try{
+        await mongoose.connect(process.env.MONGODB_URL)
+        console.log("Connected to MongoDB")
+    }catch(err){
+        console.log("Error connecting to MongoDB:",err)
+    }
+}
+
+module.exports = connectDB;
